9ddd410057a45ffc1e1f0e151788a0299b5c8799
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-04 23:13:47 +0900

    Chore: Header 분리

--- udon/src/pages/HomePage.tsx
+++ udon/src/pages/HomePage.tsx
@@ -1,9 +1,42 @@
-const HomePage = () =>{
-    return(
-        <div>
-            홈
-        </div>
-    );
-}
+import styled from "styled-components";
+import { BiSearch, BiRightIndent, BiMoon, BiWorld } from "react-icons/bi";
+import { GroupList } from "../components/GroupList"
+
+const Layout = styled.div`
+  main {
+    text-align: center;
+    background-color: blue;
+
+    ul{
+        display:flex;
+        flex-direction: column;
+        gap:10px;
+        .group-list{
+            margin: 0 auto;
+            width:80%;
+            border: 1px solid black;
+        }
+    }
+  }
+`;
+
+const HomePage = () => {
+    const test = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+  return (
+    <Layout>
+      <main>
+        <ul>
+            {test.map((items)=>{
+                return(
+                    <li className="group-list"><GroupList/></li>
+                );
+            })}
+          <li>
+          </li>
+        </ul>
+      </main>
+    </Layout>
+  );
+};
 
 export default HomePage;
