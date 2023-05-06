ebf92f04dfa1e105e513149922ee7ce60bf8c442
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-06 21:10:48 +0900

    feat: 게시판 임시디자인 적용

---
+++ udon/src/components/GroupNews.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    padding: 1rem;
+
+    .groupnews-each{
+        display:flex;
+        align-items: center;
+        gap: 1rem;
+        padding: 1rem 0;
+        border-bottom: 1px solid black;
+    }
+
+`
+
+export const GroupNews = ()=>{
+    return(
+        <Layout>
+            <div className="groupnews-each">
+                <span>{"[중요]"}</span>
+                <span>필독 공지사항</span>
+            </div>
+            <div className="groupnews-each">
+                <span>{"[중요]"}</span>
+                <span>단톡방 개설</span>
+            </div>
+        </Layout>
+    );
+}
+
