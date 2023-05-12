d6f5eba2787405d97c9809c19ddc87a7ff819ad0
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-12 21:23:26 +0900

    chore: 경로추가

--- udon/my-config.js
+++ udon/my-config.js
@@ -4,7 +4,7 @@ module.exports = {
   webpack: {
     alias: {
       '@components': path.resolve(__dirname, 'src/views/components/'),
-      '@groupdetail': path.resolve(__dirname, 'src/views/components/groupdetail'),
+      '@groupdetail': path.resolve(__dirname, 'src/views/components/groupdetail/'),
       '@pages': path.resolve(__dirname, 'src/views/pages/'),
       '@styles': path.resolve(__dirname, 'src/views/styles/'),
       
