d5581932d4bce7cf824bcb4b289c819f9d0396cc
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-14 16:57:29 +0900

    feat: Home컴포넌트 sever임시데이터 적용

--- udon/src/views/pages/HomePage.tsx
+++ udon/src/views/pages/HomePage.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { GroupList } from "@components";
 import { BaseLayout, flex_column } from '@components/AllComponent'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export interface GroupListInfo{
   id: number,
@@ -15,7 +16,6 @@ export interface GroupListInfo{
 }
 
 const Layout = styled(BaseLayout)`
-    padding:1rem;
     ul{
        ${flex_column}
         gap:10px;
@@ -25,37 +25,14 @@ const Layout = styled(BaseLayout)`
 
 
 const HomePage = () => {
-  const [groupList, setGroupList] = useState<GroupListInfo[]>([
-    {
-      id: 1,
-      region: "성남시",
-      img:"https://mblogthumb-phinf.pstatic.net/MjAyMDA0MjhfMjAz/MDAxNTg4MDQwOTA5MjE0.i6ARBaBdi_HYODiTR0f1ma8OvblzSh01bJGL8U5gDZUg.1oPlzYogIg8H_TAk5iZvUMYOLsZkZM1ITTVrfUsIHHog.JPEG.mijung011040/20200424_192926.jpg?type=w800",
-      title: "[정자역] 클라이밍 초자분들 환영 나이제한없음X",
-      people: "85",
-      detailedArea: "정자역 5번출구",
-      iskeep: false,
-    },
+  const [groupList, setGroupList] = useState<GroupListInfo[]>([]);
+  const navigate = useNavigate();
 
-    {
-      id: 2,
-      region: "서울시",
-      img:"https://img.lovepik.com/free-png/20220127/lovepik-badminton-png-image_401948661_wh1200.png",
-      title: "[서울역] 배드민턴 초자분들 환영 나이제한없음X",
-      people: "50",
-      detailedArea: "서울역 5번출구",
-      iskeep: false,
-    },
-    {
-      id: 3,
-      region: "부산시",
-      img:"https://www.hanbit.co.kr/data/editor/20191015091554_afbeoaea.png",
-      title: "[부산역] 서핑 초자분들 환영 나이제한없음X",
-      people: "100",
-      detailedArea: "부산역 5번출구",
-      iskeep: true,
-    },
-  ]);
-    const navigate = useNavigate();
+  useEffect(()=>{
+    axios.get<GroupListInfo[]>(`${process.env.REACT_APP_API_ROOT}/groupList`)
+    .then( res => setGroupList(res.data))
+    
+  },[])
 
     const clickGroup = ()=>{
         navigate("/group")
