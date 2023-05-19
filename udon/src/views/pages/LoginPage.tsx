18b5cb51ffb8167df26c69092430ba23ee1540c2
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-19 17:54:51 +0900

    feat: 로그인 컴포넌트추가

--- udon/src/views/pages/LoginPage.tsx
+++ udon/src/views/pages/LoginPage.tsx
@@ -1,10 +1,64 @@
+import styled from "styled-components";
+import { BaseLayout, colors } from "@components/AllComponent";
 
-function LoginPage(){
-    return(
-        <div>
-            로그인페이지
+const Layout = styled(BaseLayout)`
+background-color: slategrey;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
+  text-align: center;
+
+  .loginpage-info{
+    display:flex;
+    flex-direction: column;
+    gap: 1rem;
+    
+    & > input{
+        padding: 0.5rem 2rem;;
+    }
+
+    .loginpage-button{
+        flex-direction: column;
+        display:flex;
+        gap: 1rem;
+        
+
+        & > a,button{
+            border-radius: 10px;
+            padding: 1rem;
+            background-color: white;
+            font-size: 1.5rem;
+        }
+
+        & > button{
+            color: white;
+            background-color: ${ colors.buttonCreateColor};
+        }
+  }
+
+
+`;
+
+function LoginPage() {
+  return (
+    <Layout>
+        <h1>로그인</h1>
+        <div className="loginpage-info">
+          <input placeholder="아이디를 입력해주세요" />
+          <input placeholder="비밀번호를 입력해주세요" />
+          <label>
+            <input type="checkBox"/>
+            로그인 유지하기
+          </label>
+          <div  className="loginpage-button">
+            <button>로그인하기</button>
+            <a href="/signup">회원가입</a>
+            <a href="/">홈으로 가기</a>
+          </div>
         </div>
-    );
+    </Layout>
+  );
 }
 
 export default LoginPage;
