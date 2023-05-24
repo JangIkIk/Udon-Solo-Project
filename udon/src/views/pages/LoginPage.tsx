541411f0c53f1e0eff6f65cc626e3c16467c5b41
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-05-24 15:37:48 +0900

    feat: 로그인 임시기능추가

--- udon/src/views/pages/LoginPage.tsx
+++ udon/src/views/pages/LoginPage.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import { BaseLayout, colors } from "@components/AllComponent";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { Modal } from "@components/Modal"
 
 const Layout = styled(BaseLayout)`
 background-color: slategrey;
@@ -40,19 +44,54 @@ background-color: slategrey;
 
 `;
 
+interface LoginPageType{
+  loginId: string;
+  loginPassword: string;
+  keepCheck: boolean;
+}
+
 function LoginPage() {
+  const navigate = useNavigate();
+  const [isModal, setIsModal]= useState<boolean | undefined>(undefined);
+
+  const [loginData, setLoginData] = useState<LoginPageType>({
+    loginId: "",
+    loginPassword: "",
+    keepCheck: false
+  });
+
+  const handleLogin = ()=> {
+
+    axios.post(`${process.env.REACT_APP_API_ROOT}/login`,{
+      userId: loginData.loginId,
+      userPassword: loginData.loginPassword,
+      keepCheck: loginData.keepCheck
+    })
+    .then( res => {
+      console.log(res);
+      if(res.data){
+        navigate("/")
+      }else{
+        console.log(res);
+        setIsModal(res.data);
+      }
+    })
+    .catch( err => console.log(err))
+  }
+  
   return (
     <Layout>
+      {isModal === undefined ? null : <Modal check={setIsModal} children="아이디와 비밀번호를 확인해주세요"/>}
         <h1>로그인</h1>
         <div className="loginpage-info">
-          <input placeholder="아이디를 입력해주세요" />
-          <input placeholder="비밀번호를 입력해주세요" />
+          <input onChange={ (text) => setLoginData( (prev) => ({...prev, loginId: text.target.value}))} value={loginData.loginId} type="text" placeholder="아이디를 입력해주세요" />
+          <input onChange={ (text) => setLoginData( (prev) => ({...prev, loginPassword: text.target.value}))} value={loginData.loginPassword} type="password" placeholder="비밀번호를 입력해주세요" />
           <label>
-            <input type="checkBox"/>
+            <input onClick={ () => setLoginData( (prev) => ({...prev, keepCheck: !prev.keepCheck}))} type="checkBox"/>
             로그인 유지하기
           </label>
           <div  className="loginpage-button">
-            <button>로그인하기</button>
+            <button onClick={handleLogin} >로그인하기</button>
             <a href="/signup">회원가입</a>
             <a href="/">홈으로 가기</a>
           </div>
