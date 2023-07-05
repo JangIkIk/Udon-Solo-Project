f8dea26e0482bdec72e585233eb1d8a5f5990aae
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-07-05 20:31:25 +0900

    Feat: 마이페이지 타입설정 및 get요청

--- udon/src/views/pages/MyPage.tsx
+++ udon/src/views/pages/MyPage.tsx
@@ -1,16 +1,30 @@
 import styled from "styled-components";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { BiMaleFemale, BiBeenHere, BiHeart } from "react-icons/bi";
-import { useState, useEffect } from "react";
-import { GroupList, GroupListType } from "@components/GroupList";
+import axios from "axios";
+import { GroupList, GroupListType} from "@components/GroupList";
 import { 
   BaseLayout, 
   ImgBase, 
   flex_column, 
   SpanFlex,
   flex_row_all_cneter,
- } from "@components/AllComponent";
+} from "@components/AllComponent";
+import { refreshToken, isToken } from "@components/Customhooks";
+import { ProfileSettingModal } from "@components/ProfileSettingModal";
+
+interface userDataType{
+  userName: string,
+  userYears: string,
+  userGender: string,
+  userActivity: string,
+  userKeepList: GroupListType[],
+  userImage: string | null,
+  userIntroduce: string
+}
+
+
+export type OptionUserDataType = Partial<userDataType>
 
 const Layout = styled(BaseLayout)`
     .mypage-info {
@@ -35,9 +49,12 @@ const Layout = styled(BaseLayout)`
       .mypage-modify {
         ${flex_row_all_cneter}
         flex-basis: 5rem;
+        border-radius: 10px;
+        box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
         & > button {
           color: blue;
           font-size: 1.2rem;
+          cursor:pointer;
         }
       }
     }
@@ -53,32 +70,46 @@ const Layout = styled(BaseLayout)`
     }
   }
 `;
-
 function MyPage() {
-  const [groupList, setGroupList] = useState<GroupListType[]>([]);
-
-  useEffect(()=>{
-    axios.get<GroupListType[]>(`${process.env.REACT_APP_API_ROOT}/groupList`)
-    .then( res => setGroupList(res.data));
-  },[])
+  const [userData, setUserData] = useState<OptionUserDataType>({});
+  const [isModal, setIsModal] = useState<boolean>(false);
+  let a ;
+  // console.log(a)
+  useEffect(() => {
+      axios.get<OptionUserDataType>(`/mypage`,{
+          headers:{
+              Authorization: `Bearer ${isToken()}`
+          }
+      })
+      .then( res => setUserData(res.data))
+      .catch( err => {
+        console.log("오류")
+        if(err.response.status === 401){
+          return refreshToken();
+        } else{
+          console.log(err);
+        }
+      })
+  },[]);
 
   return (
     <Layout>
+      {isModal ? <ProfileSettingModal userData={userData} setUserData={setUserData} setState={setIsModal}/> : null}
       <div className="mypage-info">
         <div className="mypage-photo">
-          <ImgBase src={"https://cdn-icons-png.flaticon.com/512/3135/3135715.png"} alt="#" fit="contain"/>
+          <ImgBase src={ userData.userImage && userData.userImage !== null ? `http://localhost:4000/${userData.userImage}`: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"} alt="#" fit="contain" borderRadius={"50%"}/>
         </div>
         <div className="mypage-content">
           <div>
-            <SpanFlex>홍길동</SpanFlex>
-            <SpanFlex><BiMaleFemale /> 남</SpanFlex>
+            <SpanFlex>{userData.userName}</SpanFlex>
+            <SpanFlex><BiMaleFemale />{userData.userGender}</SpanFlex>
           </div>
           <div>
-          <SpanFlex>2000. 11. 09</SpanFlex>
-          <SpanFlex><BiBeenHere /> 경기도</SpanFlex>
+          <SpanFlex>{userData.userYears?.replaceAll("-", ".")}</SpanFlex>
+          <SpanFlex><BiBeenHere /> {userData.userActivity ?? "활동지역 없음"}</SpanFlex>
           </div>
         </div>
-        <div className="mypage-modify">
+        <div className="mypage-modify" onClick={() => setIsModal((prev) => !prev)}>
           <button>수정</button>
         </div>
       </div>
@@ -90,12 +121,11 @@ function MyPage() {
         </h3>
         <div className="mypage-keep-content">
         <ul>
-            {groupList.map((item)=>{
-              console.log(item)
+          {userData.userKeepList !== null ? userData.userKeepList?.map((item)=>{
                 return(
                   <li key={item.id}><GroupList item={item}/></li> 
                 );
-            })}
+            }) : <li>현재 찜한 그룹이 없습니다.</li>}
         </ul>
         </div>
       </div>
