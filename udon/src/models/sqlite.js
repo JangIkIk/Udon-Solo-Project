d371d62f1883da9c93a66bd10d3ff088c2e595bb
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-07-13 17:05:13 +0900

    Feat: 그룹리스트 테이블추가

--- udon/src/models/sqlite.js
+++ udon/src/models/sqlite.js
@@ -9,7 +9,7 @@ const db = new sqlite.Database(dbPath, sqlite.OPEN_READWRITE, (err) => {
 
 const createTablesQuery = `
     CREATE TABLE IF NOT EXISTS users(
-      id INTEGER PRIMARY KEY,
+      id INTEGER PRIMARY KEY, 
       userId TEXT,
       userPassword TEXT,
       userName TEXT,
@@ -21,6 +21,16 @@ const createTablesQuery = `
       userKeepList TEXT DEFAULT NULL,
       userImage TEXT DEFAULT NULL
     );
+
+    CREATE TABLE IF NOT EXISTS groupInfo(
+      id INTEGER PRIMARY KEY,
+      groupRegion TEXT,
+      groupImg TEXT,
+      groupTitle TEXT,
+      groupPeople TEXT,
+      groupDetailedArea TEXT,
+      groupContent TEXT
+    );
 `;
 
 // 테이블 생성 --- 비기능
@@ -29,7 +39,7 @@ db.exec(createTablesQuery, (err) => {
   console.log("테이블 생성");
 });
 
-//  모든테이블확인 --- 비기능
+// //  모든테이블확인 --- 비기능
 db.all(" SELECT name FROM sqlite_master WHERE type='table' ", (err, rows) => {
   if (err) {
     console.error(err);
@@ -40,11 +50,87 @@ db.all(" SELECT name FROM sqlite_master WHERE type='table' ", (err, rows) => {
   });
 });
 
-// user 테이블 데이터확인 --- 비기능
+// // user 테이블 데이터확인 --- 비기능
 db.all(`SELECT * FROM users`, [], (err, rows) => {
   if (err) return console.error(err.message);
   console.log("users:", rows);
 });
+// // groupInfo 테이블 데이터확인 --- 비기능
+// db.all(`SELECT * FROM groupInfo`, [], (err, rows) => {
+//   if (err) return console.error(err.message);
+//   console.log("groupInfo:", rows);
+// });
+
+// groupSchedule:[
+//   {
+//     id: 1,
+//     day: "11월 06일 (일)",
+//     time: "오전 11:30분",
+//     detailedArea: "안국역 원서레인즈",
+//     money: 18000,
+//     user: [
+//       {
+//         id:1,
+//         img: "https://w7.pngwing.com/pngs/949/863/png-transparent-disney-characters-disney-cartoon-character.png",
+//         years: "2000-11-09",
+//         name : "루피"
+//       },]}]
+
+const test1 = [
+  "성남시",
+  "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MjhfMjAz/MDAxNTg4MDQwOTA5MjE0.i6ARBaBdi_HYODiTR0f1ma8OvblzSh01bJGL8U5gDZUg.1oPlzYogIg8H_TAk5iZvUMYOLsZkZM1ITTVrfUsIHHog.JPEG.mijung011040/20200424_192926.jpg?type=w800",
+  "[정자역] 클라이밍 초자분들 환영 나이제한없음X",
+  "85",
+  "정자역 5번출구",
+  `안녕하세요 정자역 클라이밍입니다.
+        클라이밍을 재밌게하실분들을 모집합니다!!
+    
+        since 22.10.16
+        ♈️가입조건
+        > 활발한 활동 가능!!
+        > 나이 : 민증만있으면가능!!
+        > 왕초보 모두가능!!
+        since 22.10.16
+        ♈️가입조건
+        since 22.10.16
+        ♈️가입조건
+        > 활발한 활동 가능!!
+        > 나이 : 민증만있으면가능!!
+        > 왕초보 모두가능!!
+        since 22.10.16
+        ♈️가입조건
+        since 22.10.16
+        아무나아무나 나무나 아무나 들어오세요 ~ 
+        테스트중입니다.`,
+]
+const test2 = [
+  "서울시",
+  "https://img.lovepik.com/free-png/20220127/lovepik-badminton-png-image_401948661_wh1200.png",
+  "[서울역] 배드민턴 초자분들 환영 나이제한없음X",
+  "50",
+  "서울역 5번출구",
+  `안녕하세요 서울역 배드민턴모입입니다..
+        배드민턴을 재밌게하실분들을 모집합니다!!
+        텃세가없어요 ! 아무나와서 이용하시면됩니다
+        돈들어가는일없이 그냥 참여하세요!`,
+]
+const test3 = [
+  "부산시",
+  "https://www.hanbit.co.kr/data/editor/20191015091554_afbeoaea.png",
+  "[부산역] 서핑 초자분들 환영 나이제한없음X",
+  "100",
+  "부산역 5번출구",
+  `안녕하세요 부산 서핑하실분들 모집합니다.
+        서핑하면서 태닝도해요! 재밌게하실분들을 모집합니다!!
+        장비가 없으면 빌려드립니다.! 초보자분들 체험도 가능합니다!`,
+  
+]
+
+// 테이블에 데이터 추가
+// db.run(`INSERT INTO groupInfo (groupRegion, groupImg, groupTitle, groupPeople, groupDetailedArea, groupContent) VALUES (?, ?, ?, ?, ?, ?)`, test3, (err) => {
+//     if(err) return console.error(err.message);
+//     console.log("데이터 추가");
+// });
 
 // 테이블 삭제
 // db.run(`DROP TABLE users`);
@@ -62,6 +148,32 @@ db.all(`SELECT * FROM users`, [], (err, rows) => {
 // });
 
 
+// 그룹리스트 조회 --- 기능
+const simpleGroupList = () =>{
+  return new Promise ( (resolve, reject) => {
+    db.all("SELECT id, groupRegion, groupImg, groupTitle, groupPeople, groupDetailedArea FROM groupInfo", [], (err, row)=>{
+      if(err){
+        reject(err);
+      }else{
+        resolve(row);
+      }
+    })
+  })
+}
+// 그룹 상세정보조회 --- 기능
+const groupDetailInfo = ( groupId )=>{
+  return new Promise( (resolve, reject)=>{
+    db.get("SELECT * FROM groupInfo WHERE id = ?", [groupId], (err, row) => {
+      if(err){
+        reject(err)
+      }else{
+        
+        resolve(row)
+      }
+    })
+  })
+}
+
 // 찜리스트 조회 --- 비기능
 const myKeepList = ( userId) => {
 
@@ -76,6 +188,7 @@ const myKeepList = ( userId) => {
   })
   
 }
+
 
 // 찜리스트 추가 --- 기능
 const myKeepListAdd = ( list, userId) => {
@@ -205,6 +318,8 @@ module.exports = {
   myProfileSetting,
   myKeepListAdd,
   myKeepList,
+  simpleGroupList,
+  groupDetailInfo,
 };
 
 // db.run(`DROP TABLE myProfile`);
