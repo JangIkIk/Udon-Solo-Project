a300cc49bf582210cbf336f1e10f1ff3ed386aed
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-08-21 19:37:42 +0900

    Feat: userDayGroup추가 및 이름및 이미지 API추가

--- backend/models/users-Queries.js
+++ backend/models/users-Queries.js
@@ -33,7 +33,7 @@ const signupAdd = ({
   }) => {
     return new Promise((resolve, reject) => {
       db.run(
-        'INSERT INTO users (userId, userPassword, userName, userPhone, userYears, userGender, userActivity, userIntroduce, userKeepList, userImage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        'INSERT INTO users (userId, userPassword, userName, userPhone, userYears, userGender, userActivity, userIntroduce, userKeepList, userImage, userDayGroup) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         [
           userId,
           userPassword,
@@ -45,6 +45,7 @@ const signupAdd = ({
           userIntroduce,
           userKeepList,
           userImage,
+          userDayGroup
         ],
         (err) => {
           if (err) {
@@ -61,7 +62,7 @@ const signupAdd = ({
 const login = (userId, userPassword) => {
     return new Promise((resolve, reject) => {
       db.get(
-        'SELECT userId, userKeepList, userJoinList, userImage FROM users WHERE userId = ? AND userPassword = ? ',
+        'SELECT userId, userKeepList, userJoinList, userImage, userDayGroup FROM users WHERE userId = ? AND userPassword = ? ',
         [userId, userPassword],
         (err, row) => {
           if (err) {
@@ -78,8 +79,23 @@ const login = (userId, userPassword) => {
     });
   };
 
+  // 유저 이미지 및 이름 조회 (그룹관련)
+const groupDetaiMeetUser = ( userId )=>{
+  return new Promise((resolve, reject) => {
+    db.get('SELECT userName, userImage FROM users WHERE userId =?',userId, (err, row)=>{
+      if(err){
+        reject(err)
+      }else{
+        resolve(row)
+      }
+    })
+  })
+}
+
+
   module.exports = {
     idCheck,
     signupAdd,
-    login
+    login,
+    groupDetaiMeetUser
   };
