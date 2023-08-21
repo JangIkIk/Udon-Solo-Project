065bdefd2ad2d176f1e53346b058be69f81d50a6
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-08-21 09:44:53 +0900

    Feat: 로그인시 userImage -> redux 저장

--- frontend/src/views/pages/LoginPage.tsx
+++ frontend/src/views/pages/LoginPage.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Modal } from "@components/Modal";
 import { useAppDispatch } from "@store/store";
-import { userId, userGroup } from "@slice/groupKeepList-slice";
+import { userId, userGroup, userImage } from "@slice/userSimple-slice";
 
 const Layout = styled(BaseLayout)`
 
@@ -42,14 +42,14 @@ const Layout = styled(BaseLayout)`
 
         & > button{
             color: white;
-            background-color: ${ colors.buttonCreateColor};
+            background-color: ${colors.buttonCreateColor};
         }
   }
 
 
 `;
 
-interface LoginPageType{
+interface LoginPageType {
   loginId: string,
   loginPassword: string,
   keepCheck: boolean,
@@ -58,11 +58,11 @@ interface LoginPageType{
 function LoginPage() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch()
-  const [isModal, setIsModal]= useState<boolean | undefined>(undefined);
-  
-  
+  const [isModal, setIsModal] = useState<boolean | undefined>(undefined);
 
-  
+
+
+
 
 
 
@@ -72,58 +72,62 @@ function LoginPage() {
     keepCheck: false
   });
 
-  const handleLogin = ()=> {
-    axios.post(`/api/users/login`,{
+  const handleLogin = () => {
+    axios.post(`/api/users/login`, {
       userId: loginData.loginId,
       userPassword: loginData.loginPassword,
       keepCheck: loginData.keepCheck
-    },{withCredentials: true})
-    .then( res => {
+    }, { withCredentials: true })
+      .then(res => {
 
-      if(res.data.userKeepList){
-        const parse = JSON.parse(res.data.userKeepList);
-        const listId = parse.map( (list: { id: number }) => list.id);
-        dispatch(userGroup( {groupType: "userKeepGroup", groupId: listId} ));
-      }
-
-      if(res.data.userJoinList){
-        const parse = JSON.parse(res.data.userJoinList);
-        const listId = parse.map( (list: { id: number }) => list.id);
-        dispatch(userGroup( {groupType: "userJoinGroup", groupId: listId} ));
-      }
-      
-      if(res.data){
-        if(loginData.keepCheck){
-          const expires = new Date();
-          expires.setTime(expires.getTime() + (3600 * 1000));
-          document.cookie = `accessToken=${res.headers.authorization}; expires=${expires.toUTCString()}; path=/;`;
-        } else{
-          sessionStorage.setItem("accessToken",res.headers.authorization);
+        if(res.data.userImage){
+          dispatch(userImage(res.data.userImage)); 
         }
-        dispatch(userId(res.data.userId))
-        navigate("/");
-      }
-    })
-    .catch( () => setIsModal(false));
+        
+        if (res.data.userKeepList) {
+          const parse = JSON.parse(res.data.userKeepList);
+          const listId = parse.map((list: { id: number }) => list.id);
+          dispatch(userGroup({ groupType: "userKeepGroup", groupId: listId }));
+        }
+
+        if (res.data.userJoinList) {
+          const parse = JSON.parse(res.data.userJoinList);
+          const listId = parse.map((list: { id: number }) => list.id);
+          dispatch(userGroup({ groupType: "userJoinGroup", groupId: listId }));
+        }
+
+        if (res.data) {
+          if (loginData.keepCheck) {
+            const expires = new Date();
+            expires.setTime(expires.getTime() + (3600 * 1000));
+            document.cookie = `accessToken=${res.headers.authorization}; expires=${expires.toUTCString()}; path=/;`;
+          } else {
+            sessionStorage.setItem("accessToken", res.headers.authorization);
+          }
+          dispatch(userId(res.data.userId))
+          navigate("/");
+        }
+      })
+      .catch(() => setIsModal(false));
   }
-  
+
   return (
     <Layout>
-      {isModal === undefined ? null : <Modal check={setIsModal} children="아이디와 비밀번호를 확인해주세요"/>}
-        <div className="loginpage-info">
-          <h1>로그인</h1>
-          <input onChange={ (text) => setLoginData( (prev) => ({...prev, loginId: text.target.value}))} value={loginData.loginId} type="text" placeholder="아이디를 입력해주세요" />
-          <input onChange={ (text) => setLoginData( (prev) => ({...prev, loginPassword: text.target.value}))} value={loginData.loginPassword} type="password" placeholder="비밀번호를 입력해주세요" />
-          <label>
-            <input onClick={ () => setLoginData( (prev) => ({...prev, keepCheck: !prev.keepCheck}))} type="checkBox"/>
-            로그인 유지하기
-          </label>
-          <div  className="loginpage-button">
-            <button onClick={handleLogin} >로그인하기</button>
-            <a href="/signup">회원가입</a>
-            <a href="/">홈으로 가기</a>
-          </div>
+      {isModal === undefined ? null : <Modal check={setIsModal} children="아이디와 비밀번호를 확인해주세요" />}
+      <div className="loginpage-info">
+        <h1>로그인</h1>
+        <input onChange={(text) => setLoginData((prev) => ({ ...prev, loginId: text.target.value }))} value={loginData.loginId} type="text" placeholder="아이디를 입력해주세요" />
+        <input onChange={(text) => setLoginData((prev) => ({ ...prev, loginPassword: text.target.value }))} value={loginData.loginPassword} type="password" placeholder="비밀번호를 입력해주세요" />
+        <label>
+          <input onClick={() => setLoginData((prev) => ({ ...prev, keepCheck: !prev.keepCheck }))} type="checkBox" />
+          로그인 유지하기
+        </label>
+        <div className="loginpage-button">
+          <button onClick={handleLogin} >로그인하기</button>
+          <a href="/signup">회원가입</a>
+          <a href="/">홈으로 가기</a>
         </div>
+      </div>
     </Layout>
   );
 }
