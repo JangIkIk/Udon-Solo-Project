6328be363d23848f6e88931c25f51cfe8e20416f
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-08-26 15:29:02 +0900

    Feat: 그룹필터

--- frontend/src/views/pages/HomePage.tsx
+++ frontend/src/views/pages/HomePage.tsx
@@ -1,39 +1,80 @@
 import styled from "styled-components";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { GroupList, GroupListType } from "@components/GroupList";
-import { BaseLayout, flex_column } from '@components/AllComponent';
-import { useAppSelector } from "@store/store.ts"
+import { BaseLayout, flex_column } from "@components/AllComponent";
+import { useAppSelector } from "@store/store.ts";
 
 
 const Layout = styled(BaseLayout)`
-    ul{
-       ${flex_column};
-        gap:10px;
-    }
+  ul {
+    ${flex_column};
+    gap: 10px;
+  }
 `;
 
 const HomePage = () => {
   const [groupList, setGroupList] = useState<GroupListType[]>([]);
-  useEffect(()=>{
-    axios.get<GroupListType[]>('/api/group/list')
-    .then( res => setGroupList(res.data))
-
-  },[])
-
+  const selector = useAppSelector((state) => state.search);
   
+  
+  const filteredList = useMemo(()=>{    
+    if (selector.searchText === "") {
+      return groupList;
+    } else{
+            let result:GroupListType[] = [];
+            let tags:GroupListType[] = [];
+            let region:GroupListType[] = [];
+            let title:GroupListType[] = [];
+
+            for(let i = 0 ; i < groupList.length ; i++){
+              if(groupList[i].groupTitle.includes(selector.searchText)){
+                title.push(groupList[i]);
+                  if(selector.searchFilter.includes(groupList[i].groupTag)){
+                    tags.push(groupList[i]);
+                        if(selector.searchFilter.includes(groupList[i].groupRegion)){
+                          result.push(groupList[i]);
+                        }
+                  } else if(selector.searchFilter.includes(groupList[i].groupRegion)){
+                      region.push(groupList[i]);
+                        if(selector.searchFilter.includes(groupList[i].groupTag)){
+                          result.push(groupList[i]);
+                        }
+                  }
+              }
+            }
+            if(result.length > 0){
+              return result;
+            } else if(tags.length > 0){
+              return tags;
+            } else if(region.length > 0){
+              return region;
+            } else{
+              return title;
+            }
+    }
+
+  },[selector,groupList])
+
+  useEffect(() => {
+    if (selector.searchText === "") {
+      axios
+        .get<GroupListType[]>("/api/group/list")
+        .then((res) => setGroupList(res.data));
+    } 
+  }, []);
 
   return (
     <Layout>
       <main>
         <ul>
-            {groupList.map((item)=>{
-                return(
-                    <li key={item.id}>
-                      <GroupList item={item}/>
-                    </li>
-                );
-            })}
+          {filteredList.length !== 0 ? filteredList.map((list) => {
+            return (
+              <li key={list.id}>
+                <GroupList item={list} />
+              </li>
+            );
+          }) : <p>검색결과가 없습니다.</p>}
         </ul>
       </main>
     </Layout>
